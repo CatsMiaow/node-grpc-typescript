15c3c02032494a97b2650d05f1d86e3183f52a09
CatsMiaow <infiltrator@naver.com>
2018-12-13 19:27:47 +0900

    불필요한 파일 제거

--- dist/package/helloworld/Greeter.d.ts
+++
@@ -1,6 +0,0 @@
-import { sendUnaryData, ServerUnaryCall } from 'grpc';
-import { IGreeterServer } from '../../../models/helloworld_grpc_pb';
-import { HelloReply, HelloRequest } from '../../../models/helloworld_pb';
-export declare class Greeter implements IGreeterServer {
-    sayHello(call: ServerUnaryCall<HelloRequest>, callback: sendUnaryData<HelloReply>): void;
-}
