73d1c5a630f0c82729ca595be8f555f895603651
CatsMiaow <infiltrator@naver.com>
2019-01-14 11:13:42 +0900

    Add pm2 cluster server

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import { Server, ServerCredentials } from 'grpc';
 import { GreeterService } from '../models/helloworld_grpc_pb';
 import { Greeter } from './package/helloworld/Greeter';
 
-// grpc-tools로 proto를 가공하기 때문에 @grpc/proto-loader로 동적 로드할 필요가 없음
+// https://github.com/grpc/grpc/issues/6976
+// https://pm2.io/doc/en/runtime/guide/load-balancing/#cluster-environment-variable
+let port: number = 50051;
+if (process.env.NODE_APP_INSTANCE) {
+  port = port + Number(process.env.NODE_APP_INSTANCE);
+}
+
+// Do not use @grpc/proto-loader
 const server: Server = new Server();
 server.addService(GreeterService, new Greeter());
-server.bind('0.0.0.0:50051', ServerCredentials.createInsecure());
+server.bind(`0.0.0.0:${port}`, ServerCredentials.createInsecure());
 server.start();
